54b10a5d848d742a6fb172950123291bec15c3bd
David Corcuera <david.corcuera@gmail.com>
2023-08-08 19:55:36 +0200

    calculate tip from inputAmount

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -81,6 +81,9 @@ fun TipTimeLayoutPreview() {
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
     var amountInput by remember { mutableStateOf("") }
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount)
+
     TextField(
         value = amountInput,
         onValueChange = { amountInput = it },
